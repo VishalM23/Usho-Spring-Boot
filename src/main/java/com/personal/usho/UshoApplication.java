02bfcd661e85d2d24b824529e29376076dcfee6d
vishalm239 <vishal.m@tsgforce.com>
2025-03-10 22:21:28 +0530

    Repo Structure Initial Commit

---
+++ src/main/java/com/personal/usho/UshoApplication.java
@@ -0,0 +1,13 @@
+package com.personal.usho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UshoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/usho/UshoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.usho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
